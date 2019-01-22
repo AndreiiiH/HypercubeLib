46dd0278ff135cd8168c4aa8478542f9d9c0ef63
Andrei Hava <hava.ionut@gmail.com>
2019-01-22 13:32:14 +0000

    Initial commit

---
+++ src/main/java/andreiiih/hypercube/core/HypercubeCore.java
@@ -0,0 +1,52 @@
+/**
+ * ATTRIBUTION-NONCOMMERCIAL-NODERIVATIVES 4.0 International (CC BY-NC-ND 4.0)
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+ *
+ * You CAN:
+ *     Share           Copy and redistribute the material in any medium or format.
+ *                     The licensor cannot revoke these freedoms as long as you follow the license terms.
+ *
+ * You MUST:
+ *     Attribution     You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+ *                     You may do so in any reasonable manner, but not in any way that suggests the licensor
+ *                     endorses you or your use.
+ *
+ * You CAN'T:
+ *     NonCommercial   You may not use the material for commercial purposes.
+ *
+ *     NoDerivatives   If you remix, transform, or build upon the material, you may not distribute the modified material.
+ *
+ *     No additional   You may not apply legal terms or technological measures that legally restrict others
+ *     restrictions    from doing anything the license permits.
+ *
+ * Notices:
+ *     You do not have to comply with the license for elements of the material in the public domain or where your use
+ *     is permitted by an applicable exception or limitation.
+ *
+ *     No warranties are given. The license may not give you all of the permissions necessary for your intended use.
+ *     For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.
+ *
+ *
+ * Copyright (c) 2018-2019 Andrei Hava / AndreiiiH / AndyAndreiH <hava.ionut@gmail.com>
+ */
+package andreiiih.hypercube.core;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = HypercubeCore.ID, name = HypercubeCore.NAME, version = HypercubeCore.VERSION, useMetadata = true)
+public class HypercubeCore {
+
+    public final static String ID = "hypercubecore";
+    public final static String NAME = "Hypercube Core";
+    public final static String VERSION = "${version}";
+
+    public final static Logger LOG = LogManager.getLogger();
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        LOG.info("Success!");
+    }
+}
